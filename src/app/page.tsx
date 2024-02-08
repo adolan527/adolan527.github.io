639cdd40fbd3bd1ea9df295ddb9ca7c68d173ae0
adolan527 <adolan@ncsu.edu>
2024-02-08 00:55:52 -0500

    resume link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -163,16 +163,14 @@ export default function Component() {
             <div className="container grid items-center justify-center gap-4 px-4 md:px-6">
               <div className="space-y-3">
                 <h2 className="text-3xl font-bold tracking-tighter md:text-4xl/tight">My Resume</h2>
-                <Link href="../../public/resume.pdf">
                   <p className="max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
                     Download my resume to learn more about my experience and qualifications.
                   </p>
-                </Link>
               </div>
               <div className="flex flex-col gap-2 min-[400px]:flex-row">
                 <Link
                     className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-                    href="#"
+                    href="/resume.pdf"
                 >
                   Download Resume
                 </Link>
